3ceb62192ae9a6d98138580ee9c26b3fa742a1bb
Tim Pierce <pierce@adobe.com>
2019-12-20 20:23:55 -0500

    Add support for immediate vs. positional addressing

--- intcode.py
+++ intcode.py
@@ -3,29 +3,25 @@
 # Implementation of the Intcode computer as specified in Day 5
 
 
-from enum import Enum
-
-class Opcode(Enum):
-    ADD    =  1
-    MUL    =  2
-    INPUT  =  3
-    OUTPUT =  4
-    HALT   = 99
-
-
-InstructionSet = {
-    Opcode.ADD: {"param_count": 3},
-    Opcode.MUL: {"param_count": 3},
-    Opcode.INPUT: {"param_count": 1},
-    Opcode.OUTPUT: {"param_count": 1},
-    Opcode.HALT: {"param_count": 1},
+Opcode = {
+    1:   {"name": "ADD",    "param_count": 3},
+    2:   {"name": "MUL",    "param_count": 3},
+    3:   {"oame": "INPUT",  "param_count": 1},
+    4:   {"name": "OUTPUT", "param_count": 1},
+    99:  {"name": "HALT",   "param_count": 1},
 }
+
+
+class ParameterMode(object):
+    POSITION = 0
+    IMMEDIATE = 1
 
 
 class Instruction(object):
     def __init__(self, opcode):
         self.opcode = opcode % 100
-        self.param_count = InstructionSet[opcode]["param_count"]
+        self.name = Opcode[self.opcode]["name"]
+        self.param_count = Opcode[self.opcode]["param_count"]
 
         # Decode parameter modes from high order opcode digits
         #
@@ -36,14 +32,17 @@ class Instruction(object):
         # ten-thousands digit, and so on. Any missing modes are 0.
         
         n = opcode // 100
-        param_modes = []
+        param_mode = []
         for i in range(0, self.param_count):
-            param_modes.append(n % 10)
+            param_mode.append(n % 10)
             n = n // 10
-        self.param_modes = param_modes
-        
+        self.param_mode = param_mode
 
-        
+    def __repr__(self):
+        return "<Instruction {} param_count={} param_mode={}>".format(
+            self.name, self.param_count, self.param_mode)
+
+
 class Intcode(object):
 
     OP_ADD    =  1
@@ -59,30 +58,37 @@ class Intcode(object):
     def poke(self, addr, num):
         self.memory[addr] = num
     
-    def peek(self, addr):
+    def peek(self, addr, mode=ParameterMode.IMMEDIATE):
+        if mode == ParameterMode.POSITION:
+            addr = self.memory[addr]
         return self.memory[addr]
     
     def step(self):
         opcode = self.memory[self.pc]
-        inst = Instruction.decode(opcode)
+        inst = Instruction(opcode)
+
         param_start = self.pc + 1
-        param_end = inst.param_count
-        params = self.memory[param_start:param_end]
         
-        if opcode == Intcode.OP_ADD:
-            addr1, addr2, addr3 = self.memory[self.pc+1:self.pc+4]
-            self.memory[addr3] = self.memory[addr1] + self.memory[addr2]
-            self.pc += 4
-        elif opcode == Intcode.OP_MUL:
-            addr1, addr2, addr3 = self.memory[self.pc+1:self.pc+4]
-            self.memory[addr3] = self.memory[addr1] * self.memory[addr2]
-            self.pc += 4
-        elif opcode == Intcode.OP_HALT:
+        if inst.opcode == Intcode.OP_ADD:
+            param1 = self.peek(param_start,   mode=inst.param_mode[0])
+            param2 = self.peek(param_start+1, mode=inst.param_mode[1])
+            param3 = self.peek(param_start+2)
+            result = param1 + param2
+            self.poke(param3, result)
+        elif inst.opcode == Intcode.OP_MUL:
+            param1 = self.peek(param_start,   mode=inst.param_mode[0])
+            param2 = self.peek(param_start+1, mode=inst.param_mode[1])
+            param3 = self.peek(param_start+2)
+            result = param1 * param2
+            self.poke(param3, result)
+        elif inst.opcode == Intcode.OP_HALT:
             return False
+        else:
+            raise Exception("unknown opcode {}".format(inst.opcode))
+
+        self.pc += inst.param_count + 1
         return True
 
     def run(self):
         while self.step():
             pass
-
-
