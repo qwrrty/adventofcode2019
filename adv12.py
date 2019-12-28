08a467f594105278f8145cdb8a10c2d2e61985b5
Tim Pierce <pierce@adobe.com>
2019-12-28 14:01:27 -0500

    adding day 12 part 2 (not yet working)

--- adv12.py
+++ adv12.py
@@ -9,6 +9,8 @@ class Moon(object):
     def __init__(self, x, y, z):
         self.pos = [x, y, z]
         self.vel = [0, 0, 0]
+        self.history = [{}, {}, {}]
+        self.period = [None, None, None]
 
     def __repr__(self):
         return "<Moon pos={} vel={}>".format(self.pos, self.vel)
@@ -27,6 +29,7 @@ class System(object):
 
     def __init__(self, moons):
         self.moons = moons.copy()
+        self.steps = 0
 
     def from_text(text):
         moons = []
@@ -65,8 +68,15 @@ class System(object):
             for i in range(0,3):
                 m.pos[i] += m.vel[i]
 
+        self.steps += 1
+
     def total_energy(self):
         return sum(m.total_energy() for m in self.moons)
+
+    def print_period(self):
+        for m in self.moons:
+            print(m.period)
+        print("")
 
     
 def part1():
@@ -75,3 +85,28 @@ def part1():
         system.step()
     print(system.total_energy())
     
+def part2():
+    system = System.from_text("""<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>
+""")
+    
+    while True:
+        # Check each moon to see if any coordinate has reached a prior state
+        for m in system.moons:
+            for i in range(0,3):
+                state = (m.pos[i], m.vel[i])
+                if state in m.history[i]:
+                    if m.period[i] is None:
+                        m.period[i] = (m.history[i][state], system.steps)
+                        system.print_period()
+                else:
+                    m.history[i][state] = system.steps
+        # Have we recorded every period? If so then we're done
+        if all(type(x) is tuple for m in system.moons for x in m.period):
+            break
+        # Move forward
+        system.step()
+
+    system.print_period()
